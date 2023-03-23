b30e364fdee4177daa43c81d7e1be23363c6c07d
giorgiGrigolia <grigolialive@gmail.com>
2023-03-24 01:20:13 +0400

    Started creating the second part (quiz) of the page

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const body = document.body;
 const startButton = document.querySelector(".start-button");
 const container = document.querySelector(".container");
+const quiz = document.querySelector(".quiz");
+
+startButton.addEventListener("click", () => {
+  container.style.display = "none";
+  quiz.style.display = "block";
+});
